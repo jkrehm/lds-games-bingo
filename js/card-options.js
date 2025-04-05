b0b0c228667791904c0a14d4762a85f8f125f26f
Jonathan Rehm <jonathan@rehm.me>
2025-04-05 13:05:12 -0700

    add new options
    Also, remove duplicate "forgive".

--- js/card-options.js
+++ js/card-options.js
@@ -1,186 +1,63 @@
 export default [
-  {
-    text: "adversity",
-    image: "adversity.webp",
-  },
-  {
-    text: "agency",
-    image: "agency.webp",
-  },
-  {
-    text: "angel",
-    image: "angel.webp",
-  },
-  {
-    text: "apostasy",
-    image: "apostasy.webp",
-  },
-  {
-    text: "apostle",
-    image: "apostle.webp",
-  },
-  {
-    text: "atonement",
-    image: "atonement.webp",
-  },
-  {
-    text: "apostle",
-    image: "apostle.webp",
-  },
-  {
-    text: "authority",
-    image: "authority.webp",
-  },
-  {
-    text: "baptism",
-    image: "baptism.webp",
-  },
-  {
-    text: "believe",
-    image: "believe.webp",
-  },
-  {
-    text: "bible",
-    image: "bible.webp",
-  },
-  {
-    text: "bishop",
-    image: "bishop.webp",
-  },
-  {
-    text: "blessing",
-    image: "blessing.webp",
-  },
-  {
-    text: "book of mormon",
-    image: "book_of_mormon.webp",
-  },
-  {
-    text: "charity",
-    image: "charity.webp",
-  },
-  {
-    text: "choir",
-    image: "choir.webp",
-  },
-  {
-    text: "christlike",
-    image: "christlike.webp",
-  },
-  {
-    text: "church",
-    image: "church.webp",
-  },
-  {
-    text: "comforter",
-    image: "comforter.webp",
-  },
-  {
-    text: "commandment",
-    image: "commandment.webp",
-  },
-  // {
-  //   text: "Conference",
-  //   image: "conference.webp",
-  // },
-  // {
-  //   text: "Covenant Path",
-  //   image: "covenant_path.jpg",
-  // },
-  // {
-  //   text: "Covenants",
-  //   image: "covenants.webp",
-  // },
-  // {
-  //   text: "Disciple",
-  //   image: "disciple.webp",
-  // },
-  // {
-  //   text: "Faith",
-  //   image: "faith.webp",
-  // },
-  {
-    text: "family",
-    image: "family.webp",
-  },
-  {
-    text: "forgive",
-    image: "forgive.webp",
-  },
-  {
-    text: "Forgive",
-    image: "forgive.webp",
-  },
-  {
-    text: "gratitude",
-    image: "gratitude.webp",
-  },
-  {
-    text: "heavenly father",
-    image: "heavenly_father.webp",
-  },
-  {
-    text: "joseph smith",
-    image: "joseph_smith.webp",
-  },
-  {
-    text: "marriage",
-    image: "marriage.webp",
-  },
-  {
-    text: "missionary",
-    image: "missionary.webp",
-  },
-  {
-    text: "pioneer",
-    image: "pioneer.webp",
-  },
-  {
-    text: "plan of salvation",
-    image: "plan_of_salvation.webp",
-  },
-  {
-    text: "pray",
-    image: "pray.webp",
-  },
-  {
-    text: "priesthood",
-    image: "priesthood.webp",
-  },
-  {
-    text: "prophets",
-    image: "prophets.webp",
-  },
-  {
-    text: "repentance",
-    image: "repentance.webp",
-  },
-  {
-    text: "revelation",
-    image: "revelation.webp",
-  },
-  {
-    text: "sabbath",
-    image: "sabbath.webp",
-  },
-  {
-    text: "sacrament",
-    image: "sacrament.webp",
-  },
-  {
-    text: "second coming",
-    image: "second_coming.webp",
-  },
-  {
-    text: "temple",
-    image: "temple.webp",
-  },
-  {
-    text: "testimony",
-    image: "testimony.webp",
-  },
-  {
-    text: "tithing",
-    image: "tithing.webp",
-  },
+  { text: "adversary", image: "adversary.webp" },
+  { text: "adversity", image: "adversity.webp" },
+  { text: "agency", image: "agency.webp" },
+  { text: "amen", image: "amen.webp" },
+  { text: "angel", image: "angel.webp" },
+  { text: "apostasy", image: "apostasy.webp" },
+  { text: "apostle", image: "apostle.webp" },
+  { text: "apostle", image: "apostle.webp" },
+  { text: "armor_of_god", image: "armor_of_god.webp" },
+  { text: "atonement", image: "atonement.webp" },
+  { text: "authority", image: "authority.webp" },
+  { text: "baptism", image: "baptism.webp" },
+  { text: "believe", image: "believe.webp" },
+  { text: "benediction", image: "benediction.webp" },
+  { text: "bible", image: "bible.webp" },
+  { text: "bishop", image: "bishop.webp" },
+  { text: "blessing", image: "blessing.webp" },
+  { text: "book of mormon", image: "book_of_mormon.webp" },
+  { text: "change_of_heart", image: "change_of_heart.webp" },
+  { text: "charity", image: "charity.webp" },
+  { text: "children_youth", image: "children_youth.webp" },
+  { text: "choir", image: "choir.webp" },
+  { text: "christlike", image: "christlike.webp" },
+  { text: "church", image: "church.webp" },
+  { text: "comforter", image: "comforter.webp" },
+  { text: "commandment", image: "commandment.webp" },
+  { text: "conference", image: "conference.webp" },
+  { text: "consequence", image: "consequence.webp" },
+  { text: "discern", image: "discern.webp" },
+  { text: "disciple", image: "disciple.webp" },
+  { text: "doctrine", image: "doctrine.webp" },
+  { text: "endure_to_the_end", image: "endure_to_the_end.webp" },
+  { text: "eternity", image: "eternity.webp" },
+  { text: "faith", image: "faith.webp" },
+  { text: "family", image: "family.webp" },
+  { text: "family_history", image: "family_history.webp" },
+  { text: "forgive", image: "forgive.webp" },
+  { text: "gethsemane", image: "gethsemane.webp" },
+  { text: "gratitude", image: "gratitude.webp" },
+  { text: "heavenly father", image: "heavenly_father.webp" },
+  { text: "hope", image: "hope.webp" },
+  { text: "joseph smith", image: "joseph_smith.webp" },
+  { text: "knowledge", image: "knowledge.webp" },
+  { text: "marriage", image: "marriage.webp" },
+  { text: "ministering", image: "ministering.webp" },
+  { text: "missionary", image: "missionary.webp" },
+  { text: "pioneer", image: "pioneer.webp" },
+  { text: "plan of salvation", image: "plan_of_salvation.webp" },
+  { text: "pray", image: "pray.webp" },
+  { text: "priesthood", image: "priesthood.webp" },
+  { text: "prophets", image: "prophets.webp" },
+  { text: "repentance", image: "repentance.webp" },
+  { text: "revelation", image: "revelation.webp" },
+  { text: "sabbath", image: "sabbath.webp" },
+  { text: "sacrament", image: "sacrament.webp" },
+  { text: "second coming", image: "second_coming.webp" },
+  { text: "service", image: "service.webp" },
+  { text: "teach", image: "teach.webp" },
+  { text: "temple", image: "temple.webp" },
+  { text: "testimony", image: "testimony.webp" },
+  { text: "tithing", image: "tithing.webp" },
 ];
